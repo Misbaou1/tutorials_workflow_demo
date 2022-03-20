545d67d12e4499702f72bf938c7013a829ad1049
mamadou <bahmisbaou@gmail.com>
2022-03-20 17:40:38 -0400

    first commit

--- tutorials/tests.py
+++ tutorials/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 
+from tutorials.models import Tutorial
+
 # Create your tests here.
 
 from django.urls import reverse
@@ -11,20 +13,50 @@ def test_homepage_access():
 
 from tutorials.models import Tutorial
 
-# @pytest.mark.django_db
-# def test_create_tutorial():
+@pytest.mark.django_db
+def test_create_tutorial():
+    tutorial = Tutorial.objects.create(
+        title='Pytest',
+        tutorial_url='https://pytest-django.readthedocs.io/en/latest/index.html',
+        description='Tutorial on how to apply pytest to a Django application',
+        published=True
+    )
+    assert tutorial.title == "Pytest"
+
+@pytest.fixture
+def new_tutorial(db):
+    tutorial = Tutorial.objects.create(
+        title='Pytest',
+        tutorial_url='https://pytest-django.readthedocs.io/en/latest/index.html',
+        description='Tutorial on how to apply pytest to a Django application',
+        published=True
+    )
+    return tutorial
+
+# @pytest.fixture
+# def new_tutorial(db):
 #     tutorial = Tutorial.objects.create(
 #         title='Pytest',
 #         tutorial_url='https://pytest-django.readthedocs.io/en/latest/index.html',
 #         description='Tutorial on how to apply pytest to a Django application',
 #         published=True
 #     )
-#     assert tutorial.title == "Pytest"
+#     return tutorial
+
+
+def test_search_tutorials(new_tutorial):
+    assert Tutorial.objects.filter(title='Pytest').exists()
+
+def test_update_tutorial(new_tutorial):
+    new_tutorial.title = 'Pytest-Django'
+    new_tutorial.save()
+    assert Tutorial.objects.filter(title='Pytest-Django').exists()
+
 
 @pytest.fixture
-def new_tutorial(db):
+def another_tutorial(db):
     tutorial = Tutorial.objects.create(
-        title='Pytest',
+        title='More-Pytest',
         tutorial_url='https://pytest-django.readthedocs.io/en/latest/index.html',
         description='Tutorial on how to apply pytest to a Django application',
         published=True
@@ -39,6 +71,9 @@ def test_update_tutorial(new_tutorial):
     new_tutorial.save()
     assert Tutorial.objects.filter(title='Pytest-Django').exists()
 
+
+def test_compare_tutorials(new_tutorial, another_tutorial):
+    assert new_tutorial.pk != another_tutorial.pk
 
 @pytest.fixture
 def another_tutorial(db):
